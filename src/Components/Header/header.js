c440369661853e86760d5dc8f59fed9a94d7b8ab
Sataplata <elkhaldi.yanis@gmail.com>
2021-03-25 11:13:56 +0100

    up

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,10 +1,24 @@
+import React from "react";
 import './header.css';
+import { ReactComponent as LogoBoko } from "../../Assets/logo_boko.svg";
+
 
 export const Header = () => {
 
     return(
-        <div className="header-box">
+        <div className="header-box" id="nav">
+            <ul>
+                <div id="nav-left">
+                    <li id="bokoTitle"><LogoBoko /></li>
+                    <li><a href="#nav">BokŌ</a></li>
+                    <li id="epicerieBio">Épicerie bio</li>
+                </div>
 
+                <div id="nav-right">
+                <li id="photos"><a href="#photos">Photos</a></li>
+                <li id="contact"><a href="#contact">Contact</a></li>
+                </div>
+            </ul>
         </div>
     );
 
--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -1,10 +1,12 @@
 import './Introduction.css';
+import IntroImg from '../../Assets/intro.png';
 
 export const Introduction = () => {
 
     return(
         <div className="introduction-box">
-
+            <img className="introimg" src={IntroImg} alt="introduction" />
+            <p>BOKŌ, épicerie, concept store, ateliers, pour manger local et consommer plus responsable à Béthune.</p>
 
         </div>
     );
